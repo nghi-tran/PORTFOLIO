63210197983c61fb27c16bee89280cae34c7401d
Nghi Tran <ndtran3@asu.edu>
2022-02-26 01:43:09 -0700

    make work page template

---
+++ pages/main.js
@@ -0,0 +1,24 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML =
+    `
+    <div class="container__navbar">
+        <div class="home">
+          <a href="../index.html">nghitran</a>
+        </div>
+
+        <div class="work">
+          <a href="../work.html">work</a>
+        </div>
+
+        <div class="about">
+          <a href="../about.html">about</a>
+        </div>
+    </div>
+
+     `
+    ;
+  }
+}
+
+customElements.define('my-header', MyHeader);
